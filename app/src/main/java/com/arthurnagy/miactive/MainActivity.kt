7afc75b7960172c2091014e248a2090f9a19431e
Arthur Nagy <arthur@halcyonmobile.com>
2017-12-23 15:11:16 +0200

    Update project to android gradle plugin 3.1.0-alpha & enable DataBinding

--- app/src/main/java/com/arthurnagy/miactive/MainActivity.kt
+++ app/src/main/java/com/arthurnagy/miactive/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.arthurnagy.miactive
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: MainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        DataBindingUtil.setContentView<MainBinding>(this, R.layout.activity_main)?.let { binding = it }
     }
 }
